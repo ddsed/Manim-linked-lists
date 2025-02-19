5cfb2d1b5271cba3c9caafdf880942f0d7444103
ddsed <dashassed@gmail.com>
2025-02-19 19:15:31 +0100

    Add: insert between lines on the right side

--- my-project/insertion_snake.py
+++ my-project/insertion_snake.py
@@ -89,20 +89,23 @@ class LinkedListScene(Scene):
 
         # Add nodes and create arrows
         for i, node in enumerate(nodes):
-            self.play(FadeIn(node, run_time=0.5), FadeIn(textfuncadd, run_time=0.6))
-            self.play(FadeOut(textfuncadd, run_time=0.6))
+            self.play(FadeIn(node, run_time=0.3), FadeIn(textfuncadd, run_time=0.4))
+            self.play(FadeOut(textfuncadd, run_time=0.3))
             if i > 0:
                 row1, col1 = (i - 1) // 10, (i - 1) % 10
                 row2, col2 = i // 10, i % 10
                 arrow = nodes[i - 1].set_next(node, row1, col1, row2, col2)
-                self.play(FadeIn(arrow, run_time=0.5), FadeIn(textfuncarrow, run_time=0.6))
-                self.play(FadeOut(textfuncarrow, run_time=0.6))
+                self.play(FadeIn(arrow, run_time=0.3), FadeIn(textfuncarrow, run_time=0.4))
+                self.play(FadeOut(textfuncarrow, run_time=0.3))
 
         self.wait(1)
         # Insert a new node
-        self.insert_node_between(nodes, insert_idx1, insert_idx2, new_letter)
+        if insert_idx1 == 9 or insert_idx1 == 19:
+            self.insert_node_inbetween_lines(nodes, insert_idx1, insert_idx2, new_letter)
+        else:
+            self.insert_node_row(nodes, insert_idx1, insert_idx2, new_letter)
 
-    def insert_node_between(self, nodes, idx1, idx2, new_value):
+    def insert_node_row(self, nodes, idx1, idx2, new_value):
             # Find the reference nodes for insertion + color code them
             node1 = nodes[idx1]
             node2 = nodes[idx2]     
@@ -310,4 +313,154 @@ class LinkedListScene(Scene):
             new_node.remove_updater(update_node_position)
             new_node.next_arrow.remove_updater(update_arrow_from_new)
             node1.next_arrow.remove_updater(update_arrow_to_new)
+    
+    def insert_node_inbetween_lines(self, nodes, idx1, idx2, new_value):
+        # Find the reference nodes for insertion + color code them
+        node1 = nodes[idx1]
+        node2 = nodes[idx2]     
+
+        textfunc = Text(f"insert({node1.text.text}, {node2.text.text})", font_size = 36)
+        textfunc.next_to(nodes[0], UP, buff=0.5)
+        textfunc.align_to(nodes[0], LEFT)
+        self.play(
+            node1.box.animate.set_fill(GREEN, opacity=0.35),
+            node2.box.animate.set_fill(GREEN, opacity=0.35),
+            FadeIn(textfunc)
+        )
+
+        self.play(FadeOut(textfunc))
+
+        if not node1 or not node2:
+            print("Error: Specified nodes not found in the list.")
+            return
         
+        # Identify affected nodes for shifting to make space for insertion
+        nodes_above = [n for n in nodes if n.row <= node1.row]  # Rows before insertion
+        nodes_below = [n for n in nodes if n.row >= node2.row]  # Rows after insertion
+        
+        # Create the new node to insert
+        new_node = LinkedListNodeBasic(new_value)
+        initial_position = (node1.get_bottom() + node2.get_top()) / 2 + LEFT * 1.5
+        
+        new_node.move_to(initial_position)
+
+        self.play(
+            FadeIn(new_node), 
+            new_node.box.animate.set_fill(GREEN_E, opacity=1),
+            *[n.animate.shift(UP * 0.5) for n in nodes_above],
+            *[n.animate.shift(DOWN * 0.5) for n in nodes_below],
+            *[n.next_arrow.animate.shift(UP * 0.5) for n in nodes_above if n.next_arrow],
+            *[n.next_arrow.animate.shift(DOWN * 0.5) for n in nodes_below if n.next_arrow],
+            node1.next_arrow.animate.put_start_and_end_on(
+                node1.get_bottom() + UP * 0.5 + DOWN * 0.1, 
+                node2.get_top() + DOWN * 0.5 + UP * 0.1
+            ),
+            run_time=0.8
+        )
+
+        # Arrows to new node
+        arrow_to_new = Arrow(
+            start=node1.next_arrow.get_start(), 
+            end=new_node.get_top(),
+            tip_length=0.2,
+            buff=0.1 
+        )
+
+        new_node.next_arrow = new_node.set_next(node2, node1.row, node1.col, node2.row, node2.col)
+        self.play(Transform(node1.next_arrow, arrow_to_new), FadeIn(new_node.next_arrow))
+
+        # Shift nodes from node2 onwards
+        shifts = []
+
+        for i in range(nodes.index(node2), len(nodes)):
+            node_i = nodes[i]
+
+            # Shift node_i to the position of node_i_next
+            # The last node shift logic
+            if i == len(nodes) - 1:
+                if node_i.row % 2 != 0:
+                    shifts.append(node_i.animate.shift(LEFT * 2))
+                else:
+                    shifts.append(node_i.animate.shift(RIGHT * 2))
+            else:
+                node_i_next = nodes[i + 1]
+                shifts.append(node_i.animate.move_to(node_i_next.get_center()))
+            
+            # If node_i has a next arrow, shift it to node_i_next's arrow position
+            if node_i.next_arrow:
+                # Even lines without edge cases
+                if node_i.row % 2 == 0 and i != 8 and i != 9 and i != 28 and i != 29:
+                    shifts.append(node_i.next_arrow.animate.shift((RIGHT * 2)))
+                # From even line to odd line to become long arrow
+                elif i == 8 or i == 28:
+                    shifts.append(node_i.next_arrow.animate.put_start_and_end_on(
+                    node_i.get_bottom() + RIGHT * 2 + DOWN * 0.1, 
+                    node_i_next.get_top() + DOWN * 3 + UP * 0.1
+                ))
+                # From even line to odd line to become short arrow
+                elif i == 9 or i == 29:
+                    shifts.append(node_i.next_arrow.animate.put_start_and_end_on(
+                    node_i.get_left() + DOWN * 3 + LEFT * 0.1,  
+                    node_i_next.get_right() + LEFT * 2 + RIGHT * 0.1  
+                ))
+                # From odd line to even line to become long arrow
+                elif i == 18 or i == 38:
+                    shifts.append(node_i.next_arrow.animate.put_start_and_end_on(
+                    node_i.get_bottom() + LEFT * 2 + DOWN * 0.1, 
+                    node_i_next.get_top() + DOWN * 3 + UP * 0.1
+                ))
+                # From odd line to even line to become short arrow
+                elif i == 19 or i == 39:
+                    shifts.append(node_i.next_arrow.animate.put_start_and_end_on(
+                    node_i.get_right() + DOWN * 3 + RIGHT * 0.1,  
+                    node_i_next.get_left() + RIGHT * 2 + LEFT * 0.1  
+                ))
+                # For odd lines
+                else:
+                    shifts.append(node_i.next_arrow.animate.put_start_and_end_on(
+                    node_i.get_left() + LEFT * 2 + LEFT * 0.1,  
+                    node_i_next.get_right() + LEFT * 2 + RIGHT * 0.1
+                ))
+        
+        # Updater for arrow to new node
+        def update_arrow_to_new(arrow):
+            arrow.put_start_and_end_on(
+                node1.get_bottom() + DOWN * 0.1,  
+                new_node.get_top() + UP * 0.1 
+            )
+            
+        # Add updaters
+        node1.next_arrow.add_updater(update_arrow_to_new)
+
+        # Arrows to new node
+        arrow_from_new = Arrow(
+            start=node2.get_left(), 
+            end=node2.get_right() + LEFT * 2,
+            tip_length=0.2,
+            buff=0.1 
+        )
+
+        self.play(
+            *shifts,
+            Transform(new_node, new_node.copy().move_to(node2.get_center())),
+            Transform(new_node.next_arrow, arrow_from_new),
+            run_time=1.5
+        )
+
+        # Remove updaters
+        node1.next_arrow.remove_updater(update_arrow_to_new)
+
+        # Restore original positions of rows, new node and arrows 
+        self.play(
+            *[n.animate.shift(DOWN * 0.5) for n in nodes_above],
+            *[n.animate.shift(UP * 0.5) for n in nodes_below],
+            *[n.next_arrow.animate.shift(DOWN * 0.5) for n in nodes_above if n.next_arrow],
+            *[n.next_arrow.animate.shift(UP * 0.5) for n in nodes_below if n.next_arrow],
+            new_node.animate.shift(UP * 0.5),
+            new_node.next_arrow.animate.shift(UP * 0.5),
+            node1.next_arrow.animate.put_start_and_end_on(
+                    node1.get_bottom() + DOWN * 0.5 + DOWN * 0.1, 
+                    new_node.get_top() + UP * 0.5 + UP * 0.1
+            ),
+            run_time=0.8
+        )
