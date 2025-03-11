c0e39e98e83f90e5dc1b0e4961f3443e9adbd025
ddsed <dashassed@gmail.com>
2025-03-11 22:49:43 +0100

    Add: empty slots for memory units line

--- my-project/memory_unit.py
+++ my-project/memory_unit.py
@@ -3,9 +3,16 @@ from manim import *
 class MemoryUnit(VGroup):
     def __init__(self, value, **kwargs):
         super().__init__(**kwargs)
+        self.value = value
         self.box = Square(side_length=1, color=WHITE)
         self.line = Line(self.box.get_top(), self.box.get_bottom(), color=WHITE)
-        self.text = Text(str(value), font_size=24).move_to(self.box.get_left() + RIGHT * 0.25)
+        
+        if value is None:
+            self.text = Text("*", color=BLACK, font_size=24).move_to(self.box.get_left() + RIGHT * 0.25)
+        else:
+            self.box.set_fill(PURPLE, opacity=0.4)
+            self.text = Text(str(value), font_size=24).move_to(self.box.get_left() + RIGHT * 0.25)
+
         self.add(self.box, self.line, self.text)
         self.next_arrow = None
 
