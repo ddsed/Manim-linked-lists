bd5076f97c864a951f5e6e36672eb25cf1d79cf6
ddsed <dashassed@gmail.com>
2025-04-23 20:20:06 +0200

    Add: close up transformation into memory units

--- my-project/insertion_closeup.py
+++ my-project/insertion_closeup.py
@@ -1,4 +1,7 @@
 from manim import *
+from memory_unit import MemoryUnit
+import random
+
 # the close up look => from 2 to 4 initial nodes 
 class LinkedListNodeCloseup(VGroup):
     def __init__(self, value, **kwargs):
@@ -33,8 +36,7 @@ class LinkedListScene(Scene):
 
         insert_idx1, insert_idx2 = map(int, input(
             f"Enter the two node indices where a new node should be inserted (0-based).\n"
-            f"If you want to insert to the head – enter 0 0;\n"
-            f"If you want to insert to the tail – enter the index of the last node (={last_index}) twice: "
+            f"Valid range: 0 to {last_index}: "
         ).split())
 
         new_letter = input("Enter the new node letter: ")
@@ -71,7 +73,34 @@ class LinkedListScene(Scene):
         self.wait(1)
 
         # Insert a new node
-        self.insert_node_between(nodes, insert_idx1, insert_idx2, new_letter)
+        updated_nodes = self.insert_node_between(nodes, insert_idx1, insert_idx2, new_letter)
+
+        # Create memory units
+        memory_units = []
+
+        for i in range(10):
+            box = Square(side_length=1.8, color=WHITE, fill_opacity=0.0)
+            line = Line(box.get_top(), box.get_bottom(), color=WHITE)
+
+            unit = VGroup(box, line)
+            unit.move_to(RIGHT * i * 2)
+            memory_units.append(unit)
+
+        # Center the line on screen
+        memory_line = VGroup(*memory_units).move_to(ORIGIN)
+        memory_line.shift(DOWN * 3)
+
+        # Explanation texts
+        memory_text = Text("memory units", color=ORANGE, font_size=36)
+        memory_text.next_to(memory_units[0], UP, buff=1.3)
+        memory_text.align_to(memory_units[0], LEFT)
+        memory_labels = self.create_memory_labels(memory_units)
+
+        self.play(FadeIn(memory_line), FadeIn(memory_text), FadeIn(memory_labels))
+
+        self.convert_to_memory_units(updated_nodes, memory_units, memory_labels, memory_text)
+
+
 
     def insert_node_between(self, nodes, idx1, idx2, new_value):
         # Find the reference nodes for insertion + color code them
@@ -184,3 +213,140 @@ class LinkedListScene(Scene):
         # Remove updaters
         new_node.remove_updater(update_node_position)
         new_node.next_arrow.remove_updater(update_arrow_from_new)
+
+        shifts_final = []
+
+        for node in nodes: 
+            shifts_final.append(node.animate.shift(UP * 2.2))
+            if node.next_arrow:
+                shifts_final.append(node.next_arrow.animate.shift(UP * 2.2)) 
+        
+        shifts_final.append(new_node.next_arrow.animate.shift(UP * 2.2))
+        shifts_final.append(new_node.animate.shift(UP * 2.2))
+
+        self.play(shifts_final)
+        self.play(
+            node1.box.animate.set_fill(GREEN, opacity=0),
+            node2.box.animate.set_fill(GREEN, opacity=0),
+            new_node.box.animate.set_fill(GREEN, opacity=0),
+        )
+
+        # Find the position to insert
+        insert_index = nodes.index(node1) + 1
+
+        # Insert the new node into the list
+        nodes.insert(insert_index, new_node)
+
+        return nodes
+    
+    def create_memory_labels(self, memory_units, start_address=452):
+        memory_labels = VGroup()
+
+        current_number = start_address
+        for unit in memory_units:
+            # Create a memory unit number above each unit
+            memory_label = Text(str(current_number), color=ORANGE, font_size=24)
+            memory_label.next_to(unit, UP, buff=0.8)
+
+            memory_labels.add(memory_label)
+            current_number += 1
+
+        return memory_labels
+
+    def convert_to_memory_units(self, nodes, memory_units, memory_labels, memory_text):
+        used_indices = set()
+        node_texts_in_memory = []
+        node_indices = []
+
+        for i in range(len(nodes)):
+            # Pick random memory slots
+            while True:
+                idx1 = random.randint(0, len(memory_units) - 1)
+                if idx1 not in used_indices:
+                    used_indices.add(idx1)
+                    break
+
+            target = memory_units[idx1].get_center() + LEFT * 0.45
+
+            # Move the node text only (no more animations on the text after it's moved)
+            node_text = nodes[i].text
+
+            # Detach text from VGroups and animate to the memory unit positions
+            node_text.generate_target()
+            node_text.target.move_to(target)
+
+            self.play(nodes[i].box.animate.set_fill(PURPLE, opacity=0.35))
+
+            # Create an index label above the memory unit
+            circle = Circle(radius=0.2, color=PURPLE, fill_opacity=1)
+            index_label = Text(str(i), color=WHITE, font_size=24)
+            index_label.move_to(circle.get_center())
+            index_with_bg = VGroup(circle, index_label)
+            index_with_bg.next_to(memory_units[idx1], UP, buff=0.2)
+
+            
+            # Move the text of node i to memory
+            self.play(
+                MoveToTarget(node_text),
+                memory_units[idx1][0].animate.set_fill(PURPLE, opacity=0.35),
+                FadeIn(index_with_bg),
+                FadeOut(nodes[i].box),
+                FadeOut(nodes[i].line),
+                FadeOut(nodes[i].dot),
+                FadeOut(nodes[i].next_arrow) if nodes[i].next_arrow else Wait(0),
+            )
+            node_texts_in_memory.append(node_text)
+            node_indices.append(index_with_bg)
+            self.wait(0.2)
+        self.play(
+            VGroup(*memory_units).animate.shift(UP * 3),
+            VGroup(*node_texts_in_memory).animate.shift(UP * 3),
+            VGroup(*node_indices).animate.shift(UP * 3),
+            VGroup(*memory_labels).animate.shift(UP * 3),
+            memory_text.animate.shift(UP * 3)
+        )
+
+        self.create_arrows(node_texts_in_memory)
+
+    def create_arrows(self, node_texts_in_memory):
+        arrows = VGroup()
+
+        for i in range(len(node_texts_in_memory) - 1):
+            start_text = node_texts_in_memory[i].get_center() + RIGHT * 0.9 + DOWN * 0.9
+            end_text = node_texts_in_memory[i + 1].get_center() + DOWN * 0.9
+
+            # Get the x-coordinate positions of the current node and next node
+            current_x = node_texts_in_memory[i].get_center()[0]
+            next_x = node_texts_in_memory[i + 1].get_center()[0]
+
+            if current_x < next_x:
+                # Create an arrow
+                arrow = CurvedArrow(
+                    start_point=start_text, 
+                    end_point=end_text,
+                    angle=TAU/4,
+                    tip_length=0.2
+                )
+            else:
+                # Create an arrow
+                arrow = CurvedArrow(
+                    start_point=start_text, 
+                    end_point=end_text,
+                    angle=-TAU/4,
+                    tip_length=0.2
+                )
+            arrows.add(arrow)
+
+        # Optional: add a dot to the last one to indicate "end"
+        end_dot = Circle(radius=0.15, color=WHITE, fill_opacity=1)
+        end_dot.move_to(node_texts_in_memory[-1].get_center() + RIGHT * 0.9)
+        arrows.add(end_dot)
+
+        for i, arrow in enumerate(arrows):
+            if i < 3:
+                self.play(FadeIn(arrow, run_time=0.5))
+                self.wait(0.1)
+            else:
+                self.play(FadeIn(arrow, run_time=0.2))
+
+        return arrows
