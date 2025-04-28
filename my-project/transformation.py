29e788790ea88cd999b8b8252933f17700fcfe9e
ddsed <dashassed@gmail.com>
2025-04-28 12:00:01 +0200

    Refactor: transformation uses same closeup node

--- my-project/transformation.py
+++ my-project/transformation.py
@@ -1,25 +1,7 @@
 from manim import *
 from memory_unit import MemoryUnit
+from node_closeup import LinkedListNodeCloseup
 import random
-
-class LinkedListNodeCloseup(VGroup):
-    def __init__(self, value, **kwargs):
-        super().__init__(**kwargs)
-        self.box = Square(side_length=2, color=WHITE)
-        self.line = Line(self.box.get_left(), self.box.get_right(), color=WHITE)
-        self.text = Text(value, font_size=34).move_to(self.box.get_top() - [0, 0.5, 0])
-        self.dot = Circle().scale(0.2)
-        self.dot.set_fill(WHITE, opacity=1)
-        self.dot.move_to(self.box.get_bottom() + [0 , 0.5, 0])
-        self.dot.set_stroke(width=0) 
-        self.add(self.box, self.text, self.line, self.dot)
-        self.next_arrow = None
-
-    def set_next(self, next_node):
-        if self.next_arrow:
-            self.remove(self.next_arrow)
-        self.next_arrow = Arrow(self.get_bottom() + [0, 0.5, 0], next_node.get_left() + [0, 0.5, 0], buff=0.1, tip_length = 0.2, color=WHITE)
-        return self.next_arrow
 
 class LinkedListScene(Scene):
     def construct(self):
@@ -59,7 +41,7 @@ class LinkedListScene(Scene):
             self.play(FadeIn(node, run_time=0.4), FadeIn(textfuncadd, run_time=0.4))
             self.play(FadeOut(textfuncadd, run_time=0.4))
             if i > 0:
-                self.play(FadeIn(nodes[i - 1].set_next(node), run_time=0.4), FadeIn(textfuncarrow, run_time=0.4))
+                self.play(FadeIn(nodes[i - 1].set_next(node, 0, 0), run_time=0.4), FadeIn(textfuncarrow, run_time=0.4), nodes[i-1].next_arrow.animate.shift(UP * 0.2))
                 self.play(FadeOut(textfuncarrow, run_time=0.4))
         
         self.wait(1)
